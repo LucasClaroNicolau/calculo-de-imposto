57f815c586f23125931b9e096a9d7eea87d66501
Lucas Claro Nicolau <lucasclaro10@hotmail.com>
2024-01-25 19:34:01 -0300

    Commit Inicial

---
+++ src/main/java/com/projeto/calculodeimposto/calculodeimposto/CalculoDeImpostoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.calculodeimposto.calculodeimposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoDeImpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoDeImpostoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/calculodeimposto/calculodeimposto/CalculoDeImpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.calculodeimposto.calculodeimposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoDeImpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
